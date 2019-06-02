966b2f430ef54cff521452ac6f687d2141f7851b
Yanliang <18652550182@163.com>
2019-06-02 21:29:12 +0800

    springboot搭建

---
+++ src/main/java/com/mailbackstage/MailBackStageApplication.java
@@ -0,0 +1,13 @@
+package com.mailbackstage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailBackStageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailBackStageApplication.class, args);
+	}
+
+}
